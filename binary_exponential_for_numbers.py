0d61c49b21d4ab2147db7b95d51b5b16a7233181
Liza Pivovarova <liza.pif@mail.ru>
2019-03-18 22:51:34 +0300

    update 18.03.19

---
+++ binary_exponential_for_numbers.py
@@ -0,0 +1,31 @@
+
+
+def binary_pow_recursive(a, b):
+    if (b==0):
+        return 1;
+    if (b % 2==0):
+        temp = binary_pow(a, int(b/2))
+        return temp*temp;
+    if (b%2 == 1):
+        return binary_pow(a, b-1)*a
+
+def binary_pow(a, b): #3, 7=111
+    
+    res = 1
+    if (b==0)&(a==0):
+        return None
+    while( b>0):
+        
+       if (b&1):
+            res *= a
+        a = a*a
+        b = b>>1
+            
+    return res   
+    
+print("введите число")
+a = input()
+print("введите степень")
+
+b = input()
+print(a, " ^ ", b, " = ", binary_pow(int(a), int(b)))
--- karatsuba_for_numbers.py
+++ karatsuba_for_numbers.py
@@ -1,75 +1,37 @@
-# Алгоритм Карацубы ( для чиселок и многочленов )
-# 305*485
-# (03+05)(04+85)
-#
-#def Karatsuba(a, b):
-
-
-def multiply(str1, str2, length):
-    
-    if (length<2):
-        return int(str1)* int(str2)
-    else:
-        return Karatsuba(str1, str2)
-
-
-def degree(string, n): #дополнение нулями n раз
-    
-    while (n>0):
-        string = "0" + string
-        n = n-1
-        
-    return string
-    
-
-
 
 def Karatsuba(a, b):
-
-    a_len = len(a)
-    b_len = len(b)
-
-    if (a_len < b_len): #берем разряд a_len как наибольщий
-        temp = a_len
-        a_len = b_len
-        b_len = temp
-        
-    if (a_len & 1): #если нечетное
-        a = degree(a, 1)
-        a_len += 1
-
-    #дополняем нулями вторую строку
-    b = degree(b, a_len - b_len)
     
-    #print("a = ", a, " , b = ", b)
+    alen = len(str(a))
+    blen = len(str(b))
+    length = max(alen, blen)
+
+    if (length<2):
+        return a*b
     
-    a_len = int(a_len/2)
-                
-    a1 = a[:a_len]
-    a2 = a[a_len:]
-    b1 = b[:a_len]
-    b2 = b[a_len:]
-                
-    temp_mult = multiply(a1,b2, a_len)+multiply(a2,b1, a_len)
+    if (length&1):
+        length = length+1
     
+    length = length//2
+    
+    al = a//10**length
+    ar = a%10**length
+    
+    bl = b//10**length
+    br = b%10**length
 
-    res = pow(10, a_len*2)*multiply(a1,b1, a_len) + pow(10, a_len)*temp_mult + multiply(a2,b2, a_len)
+    p1 = Karatsuba(al, bl)
+    p2 = Karatsuba(ar,br)
 
-    return res
-        
+    return p1*10**(length*2)+p2+(Karatsuba(al+ar, bl+br)-p1-p2)*10**length
 
 
-
-#return string[int(len(string)/2):]
 a = input()
 b = input()
-c = Karatsuba(a, b)
+c = Karatsuba(int(a), int(b))
                 
 print (a, "*", b, " = ", c)
     
 
-
-#print("остаток от деления", a_count%2)
 
     
         
